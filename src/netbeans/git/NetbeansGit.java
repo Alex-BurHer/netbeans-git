73db9ef981e6eed6a94d31b27bdc82afae0cb31f
Alex-BurHer <alex.burher@educa.jcyl.es>
2025-02-26 20:04:06 +0100

    Añadido mensajes y Clase Persona

--- src/netbeans/git/NetbeansGit.java
+++ src/netbeans/git/NetbeansGit.java
@@ -4,6 +4,8 @@ public class NetbeansGit {
 
     public static void main(String[] args) {
         System.out.println("Hola");
+        System.out.println("Adios");
+        System.out.println("Cerrando");
     }
     
 }
